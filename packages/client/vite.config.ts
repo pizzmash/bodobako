ba45e18702e5fb7d32abe5ae9c64f80ddc1bc2a3
pizzmash <music_xxx_sfy@yahoo.co.jp>
2026-02-12 01:23:21 +0900

    chore: allowdHosts設定追加

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: "all",
     proxy: {
       "/socket.io": {
         target: "http://localhost:3001",
